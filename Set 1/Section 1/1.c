72277383af2d1cf6f1e7f42e6cad8308eb93f9f4
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-03-03 23:30:08 +0530

    Create 1.c

---
+++ Set 1/Section 1/1.c
@@ -0,0 +1,39 @@
+// Remove loop in Linked List //
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void removeLoop(struct Node* head) {
+    if (head == NULL || head->next == NULL) return;
+
+    struct Node *slow = head, *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            break; 
+        }
+    }
+
+    if (slow == fast) {
+        slow = head;
+
+        if (slow == fast) {
+            while (fast->next != slow) {
+                fast = fast->next;
+            }
+        } else {
+            while (slow->next != fast->next) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+        fast->next = NULL;
+    }
+}
